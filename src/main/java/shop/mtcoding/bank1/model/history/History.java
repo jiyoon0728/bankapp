692a0b919dcd7a7e6a9b09690158227ad3b7ff06
unknown <kimjiyoon0728@naver.com>
2023-03-14 16:51:45 +0900

    db설계완료 레파지토리 완료

---
+++ src/main/java/shop/mtcoding/bank1/model/history/History.java
@@ -0,0 +1,18 @@
+package shop.mtcoding.bank1.model.history;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class History {
+    private Integer id;
+    private Long amount;
+    private Long wBalance; // 출금계좌 잔액
+    private Long dBalance; // 입금계좌 잔액
+    private Integer wAccountId;
+    private Integer dAccountId;
+    private Timestamp createdAt;
+}
